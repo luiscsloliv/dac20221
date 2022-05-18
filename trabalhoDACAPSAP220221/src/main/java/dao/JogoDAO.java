84f90aaf44f57b5f9272924fbfa549efb5c4f6b9
luiscsloliv <81023321+luiscsloliv@users.noreply.github.com>
2022-05-18 10:29:53 -0300

    pkg dao, class JogoDAO
    Adicionado também as classes JPAUtil e o persistence.xml

---
+++ trabalhoDACAPSAP220221/src/main/java/dao/JogoDAO.java
@@ -0,0 +1,43 @@
+package dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import entidade.Jogo;
+import util.JPAUtil;
+
+public class JogoDAO {
+	public static void salvar(Jogo j) {
+		EntityManager em = JPAUtil.criarEntityManager();
+		em.getTransaction().begin();
+		em.persist(j);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public static void editar(Jogo j) {
+		EntityManager em = JPAUtil.criarEntityManager();
+		em.getTransaction().begin();
+		em.merge(j);
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	public static void apagar(Jogo j) {
+		EntityManager em = JPAUtil.criarEntityManager();
+		em.getTransaction().begin();
+		j = em.find(Jogo.class, j.getId());
+		em.remove(j);
+		em.getTransaction().commit();
+		em.close();
+	}
+	public static List<Jogo> listar(){
+		EntityManager em = JPAUtil.criarEntityManager();
+		Query q = em.createQuery("select j from Jogo j");
+		List<Jogo> j = q.getResultList();
+		em.close();
+		return j;
+	}
+}
