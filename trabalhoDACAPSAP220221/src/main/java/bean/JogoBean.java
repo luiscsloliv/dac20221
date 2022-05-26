621bd0c5d5b028a02be6fec86d9fb62f26dd930e
luiscsloliv <81023321+luiscsloliv@users.noreply.github.com>
2022-05-26 10:19:54 -0300

    Alteração no DAO
    alterado algumas informações no dao.

---
+++ trabalhoDACAPSAP220221/src/main/java/bean/JogoBean.java
@@ -0,0 +1,71 @@
+package bean;
+
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import org.primefaces.PrimeFaces;
+
+import dao.JogoDAO;
+import entidade.Jogo;
+
+@ManagedBean
+public class JogoBean {
+	private Jogo jogo = new Jogo();
+	private List<Jogo> lista;
+	
+	public String salvar() {
+		try {
+			JogoDAO.salvar(jogo);
+			jogo = new Jogo();
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso", "Jogo criado com sucesso!"));
+			
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Erro", "Jogo não foi criado!"));
+		}
+		return null;
+	}
+	public String editar() {
+		try {
+			JogoDAO.editar(jogo);
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso", "Jogo editado com sucesso!"));
+			
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Erro", "Jogo não foi editado!"));
+		}
+		return "listagem";
+	}
+	public String apagar() {
+		try {
+			JogoDAO.apagar(jogo);
+			lista.remove(jogo);
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso", "Jogo excluído com sucesso!"));
+			
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Erro", "Jogo não foi excluído!"));
+		}
+		return null;
+	}
+	public String buscaPares() {
+		List<Jogo> l = JogoDAO.buscaPares(jogo);
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Atenção", "Existem " + l.toString() + " registros!");
+	    PrimeFaces.current().dialog().showMessageDynamic(message);
+		return null;
+		
+	}
+	public Jogo getJogo() {
+		return jogo;
+	}
+	public void setJogo(Jogo jogo) {
+		this.jogo = jogo;
+	}
+	public List<Jogo> getLista() {
+		if (lista == null)
+			lista = JogoDAO.listar();
+		return lista;
+	}
+	public void setLista(List<Jogo> lista) {
+		this.lista = lista;
+	}
+	
+}
--- trabalhoDACAPSAP220221/src/main/java/dao/JogoDAO.java
+++ trabalhoDACAPSAP220221/src/main/java/dao/JogoDAO.java
@@ -40,4 +40,31 @@ public class JogoDAO {
 		em.close();
 		return j;
 	}
+	
+	public static List<Jogo> buscaPares(Jogo j){
+		EntityManager em = JPAUtil.criarEntityManager();
+		//em.getTransaction().begin();
+		//j = em.find(Jogo.class, j.getId());
+		Query q = em.createQuery("select case when j.v1 % 2 = 0 then j.v1 end as v1,"
+										+ "case when j.v2 % 2 = 0 then j.v2 end as v2,"
+										+ "case when j.v3 % 2 = 0 then j.v3 end as v3,"
+										+ "case when j.v4 % 2 = 0 then j.v4 end as v4,"
+										+ "case when j.v5 % 2 = 0 then j.v5 end as v5,"
+										+ "case when j.v6 % 2 = 0 then j.v6 end as v6,"
+										+ "case when j.v7 % 2 = 0 then j.v7 end as v7,"
+										+ "case when j.v8 % 2 = 0 then j.v8 end as v8,"
+										+ "case when j.v9 % 2 = 0 then j.v9 end as v9,"
+										+ "case when j.v10 % 2 = 0 then j.v10 end as v10 "
+										+ "from Jogo j");
+		List<Jogo> lPares = q.getResultList();
+		em.close();
+		return lPares;
+	}
+	
+	
+	/*public static Query pares(){
+		EntityManager em = JPAUtil.criarEntityManager();
+		Query pares = em.createQuery("select case when j.v1 % 2 = 0 then j.v1 end as v1,case when j.v2 % 2 = 0 then j.v2 end as v2,case when j.v3 % 2 = 0 then j.v3 end as v3,case when j.v4 % 2 = 0 then j.v4 end as v4,case when j.v5 % 2 = 0 then j.v5 end as v5,case when j.v6 % 2 = 0 then j.v6 end as v6,case when j.v7 % 2 = 0 then j.v7 end as v7,case when j.v8 % 2 = 0 then j.v8 end as v8,case when j.v9 % 2 = 0 then j.v9 end as v9,case when j.v10 % 2 = 0 then j.v10 end as v10 from Jogo j");
+		return pares;
+	}*/
 }
